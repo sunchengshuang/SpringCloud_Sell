e2651b57cbf36c80f1f7cee78e65e46b137f7aa0
DESKTOP-NA2OPPJ\hasee <1501564610@qq.com>
2019-09-07 23:13:43 +0800

    config配置自动刷新完成

---
+++ order/order-server/src/main/java/com/imooc/order/config/GirlConfig.java
@@ -0,0 +1,20 @@
+package com.imooc.order.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ */
+@Data
+//@Component
+@ConfigurationProperties("girl")
+@RefreshScope
+public class GirlConfig {
+
+    private String name;
+
+    private String age;
+}
