c37cfe31545e645823fb520363870988a32ad49f
sunchengshuang <1501564610@qq.com>
2020-02-14 14:57:11 +0800

    重做系统后 config启动成功

--- config/src/test/java/com/imooc/config/ConfigApplicationTests.java
+++ config/src/test/java/com/imooc/config/ConfigApplicationTests.java
@@ -1,7 +1,8 @@
 package com.imooc.config;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
 
 @SpringBootTest
 class ConfigApplicationTests {
