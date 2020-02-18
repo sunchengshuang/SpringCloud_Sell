d2ca70216faceb00483318e06672688af8822cdb
sunchengshuang <1501564610@qq.com>
2020-02-18 15:42:53 +0800

    Jenkins第一次测试

--- eureka/deploy.sh
+++ eureka/deploy.sh
@@ -18,34 +18,6 @@ killTomcat()
    fi
 }
 cd $PROJ_PATH/SpringCloud_Sell/eureka # 修改
-mvn clean install
-
-# 停tomcat
-killTomcat
-
-# 删除原有工程
-rm -rf TOMCAT_APP_PATH/webapps/ROOT
-rm -f TOMCAT_APP_PATH/webapps/ROOT.war
-rm -f TOMCAT_APP_PATH/webapps/eureka.war
-
-# 复制新的工程
-cp $PROJ_PATH/SpringCloud_Sell/eureka/target/eureka.war $TOMCAT_APP_PATH/webapps/ # 修改
-
-cd $TOMCAT_APP_PATH/webapps/
-mv eureka.war ROOT.war
-
-# 启动tomtat
-cd $TOMCAT_APP_PATH/
-sh bin/startup.sh
-#!/usr/bin/env bash
-#编译+部署eureka站点
-#需要配置如下参数
-# 项目路径，在Excute Shell 中配置项目路径，pwd 就可以获得该项目的路径
-# export PROJ_PATH=这个jenkins任务在部署机器上的路径
-# 输入你环境上的tomtat的全路径
-# export TOMCAT_APP_PATH=tomcat在部署机器上的路径
-### base 函数
-cd $PROJ_PATH/SpringCloud_Sell/eureka # 修改
 #mvn clean install
 mvn clean package install '-Dmaven.test.skip=true'
 # 停tomcat
