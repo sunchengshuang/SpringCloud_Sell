7aa8812af9caac1378925037c0bd2f6b322da43f
兔子啃骨头 <1501564610@qq.com>
2020-02-18 12:07:42 +0800

    update eureka/deploy.sh.

--- eureka/deploy.sh
+++ eureka/deploy.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 # 编译+部署eureka站点
-
 # 需要配置如下参数
 # 项目路径，在Excute Shell 中配置项目路径，pwd 就可以获得该项目的路径
 # export PROJ_PATH=这个jenkins任务在部署机器上的路径
 
 # 输入你环境上的tomtat的全路径
 # export TOMCAT_APP_PATH=tomcat在部署机器上的路径
-
 ### base 函数
 killTomcat()
 {
